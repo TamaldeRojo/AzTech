5d94b1bed1fb9964e0210736a1394b4f0c71cb74
josue <josue.medina9991@gmail.com>
2023-11-04 15:37:59 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.css"
+import Landing from "./pages/Landing"
+import Navbar from "./components/Navbar"
+function App() {
+ 
+  return (
+    <>
+    <Navbar/>
+    <Landing/>
+    </>
+  )
+}
+
+export default App
